b51a51ad77d406682d256bf19d31c03e2e198a5d
Raphael Chevallier <raphaelchevallier@hotmail.com>
2020-01-18 14:30:34 -0800

    google maps set up

--- good_neighbors/ios/Runner/AppDelegate.swift
+++ good_neighbors/ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    [GMSServices provideAPIKey: @"AIzaSyDWVlWGQDXtfWjXSphFXiDLA4-zr7zyGu0"];
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
