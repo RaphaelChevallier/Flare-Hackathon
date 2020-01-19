adc1ae77934a874337baf689a11127e37b6b7d5a
clhumain <mdasilva311@gmail.com>
2020-01-18 23:26:25 -0800

    Fixed main.dart connection to profile.dart

--- good_neighbors/lib/home_widget.dart
+++ good_neighbors/lib/home_widget.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'placeholder_widget.dart';
 import 'google_maps_view.dart';
-<<<<<<< HEAD
 import 'requests.dart';
-=======
 import 'profile.dart';
->>>>>>> 1a8c1a2f631e5d1790e5ac2f4d800a634ca8d075
 
 class Home extends StatefulWidget {
   @override
@@ -44,15 +41,9 @@ class _HomeState extends State<Home> {
             title: Text('Rewards'),
           ),
           BottomNavigationBarItem(
-<<<<<<< HEAD
              icon: Icon(Icons.person),
              title: Text('Profile')
            )
-=======
-           icon: Icon(Icons.person),
-            title: Text('Profile')
-          )
->>>>>>> 1a8c1a2f631e5d1790e5ac2f4d800a634ca8d075
         ],
       ),
     );
